f76a8293a77d4cbdfba66e960b13d25f970e52ae
Ahemad Shaikh <ahemadshaikh8921@gmail.com>
2023-02-05 15:27:47 +0530

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,57 @@ import { useEffect, useState } from 'react'
 import axios from "axios";
 import { FaSearchLocation } from "react-icons/fa";
 import { IoPartlySunnyOutline } from "react-icons/io5";
-
+import moment from 'moment-timezone';
 
 const Index = () => {
   const [location, setLocation] = useState({});
 
-  const [data, setData] = useState(); 
+  const [data, setData] = useState();
 
-  const [temp, setTemp] = useState(data && data.temp);
+  const [currentTime, setCurrentTime] = useState("");
+
+  const [theme, setTheme] = useState('');
+
+
+  useEffect(() => {
+    // function to get user current location & show the current time
+    const getUserTime = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const date = new Date();
+          const hour = date.getHours();
+          const minute = date.getMinutes();
+          setCurrentTime(`${hour}:${minute}`);
+          if((hour>='18') && (minute>='00' || minute>='0' )) {
+            setTheme('data-theme')
+          }
+        },
+        (error) => {
+          // console.error(error);
+        }
+      );
+    };
+
+    getUserTime();
+  }, []);
+
+  const [cityPre, setCityPre] = useState(null);
+  const [city, setCity] = useState(null);
+
+  const getCityName = (c) => {
+    console.log(c, "c")
+    setCityPre(c);
+  }
+
+  function getDataAsPerCITY() {
+    setCity(cityPre);
+  }
+
 
   // function to check current address 
   function checkMyWeather() {
+    setCity(setLocation.city);
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         const { latitude, longitude } = position.coords;
@@ -23,7 +63,7 @@ const Index = () => {
           .then(res => res.json())
           .then(data => {
             setLocation({
-              city: data.address.town || data.address.city || data.address.state,
+              city: city ? city : (data.address.town || data.address.city || data.address.state),
               country: data.address.country
             });
           });
@@ -32,40 +72,49 @@ const Index = () => {
 
   }
 
-  const [city, setCity] = useState(location && location.city);
-
-  console.log(data && data, "city")
 
   // getting my current location ( city & country )
   useEffect(() => {
     checkMyWeather();
-  }, []);
+  }, [city]);
 
-    const options = {
-      method: 'GET',
-      url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
-      params: { city: `${location.city && location.city}` },
-      headers: {
-        'X-RapidAPI-Key': 'e07e13eeebmsh9d9702b3c449eefp19f07ajsn5dea798f39e1',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-      }
-    };
+  // api for getting weather data
+  const options = {
+    method: 'GET',
+    url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+    params: { city: `${location.city && location.city}` },
+    headers: {
+      'X-RapidAPI-Key': 'e07e13eeebmsh9d9702b3c449eefp19f07ajsn5dea798f39e1',
+      'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+  };
+  axios.request(options).then(function (response) {
+    // console.log(response.data);
+    setData(response && response.data)
+  }).catch(function (error) {
+    // console.error(error);
+  });
+
+
+  function changeTheme () {
+   if(theme == "data-theme"){
+    setTheme(null);
+   }
+   else{
+     setTheme('data-theme');
+   }
+  }
+
   
-    axios.request(options).then(function (response) {
-      // console.log(response.data);
-      setData(response && response.data)
-    }).catch(function (error) {
-      console.error(error);
-    });
+  // const checkbox = document.getElementById('checkbox');
 
-
-
-
-
+  // checkbox.addEventListener('change', () => {
+  //   document.body.classList.toggle('dark');
+  // })
 
   return (
     <>
-      <main className='container'>
+      <main className={`'container'+ ${theme}`}>
 
         {/* weather app body start */}
         <div className="row  d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
@@ -75,8 +124,8 @@ const Index = () => {
 
               {/* inputs for searching location */}
               <div className="search-country d-flex align-items-center justify-content-between">
-                <input type="text" placeholder='Another location ?' />
-                <div className="submit-icon">
+                <input onChange={(x => getCityName(x.target.value))} type="text" placeholder='Another location ?' />
+                <div onClick={getDataAsPerCITY} className="submit-icon">
                   <FaSearchLocation />
                 </div>
               </div>
@@ -87,10 +136,10 @@ const Index = () => {
                 <div className="country-time d-flex align-items-center justify-content-between">
 
                   {/* weather country name */}
-                  <div className="country-name"><h6>{location ? location.city : 'loading...'}</h6></div>
+                  <div className="country-name"><h6>{city ? city : (location ? location.city : 'loading...')}</h6></div>
 
                   {/* current time */}
-                  <div className="current-time"><h6>10:25</h6></div>
+                  <div className="current-time"><h6>{currentTime}</h6></div>
 
                 </div>
 
@@ -103,13 +152,21 @@ const Index = () => {
 
                 <div className="row additional">
                   <div className="col-8">
-                    <div className="data-1">{data ? data && data.wind_speed : 'loading...' } km/h</div>
-                    <div className="data-2">{data ? data.wind_degrees : 'loading...' } %</div>
+                    <div className="data-1">{data ? data && data.wind_speed : 'loading...'} km/h</div>
+                    <div className="data-2">{data ? data.wind_degrees : 'loading...'} %</div>
                     <div className="data-3">{data ? data.humidity : 'loading...'} h</div>
                   </div>
                   <div className="col-4">
-                    <div className="tempreture d-flex align-items-center justify-content-center">
+                    <div className="tempreture d-flex flex-column align-items-center justify-content-center">
                       <h1>{data ? data.temp : '...'}<sup>o</sup></h1>
+                      <div className='pt-3'>
+                        <input onClick={changeTheme} type="checkbox" class="checkbox" id="checkbox" />
+                        <label for="checkbox" class="label">
+                          <i class="fas fa-moon"></i>
+                          <i class='fas fa-sun'></i>
+                          <div class='ball'></div>
+                        </label>
+                      </div>
                     </div>
                   </div>
                 </div>
